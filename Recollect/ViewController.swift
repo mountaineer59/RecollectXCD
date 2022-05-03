98a809404e97a1f34f760e730594b0851ac4788b
sourabhwasnik <sourabh29wasnik@gmail.com>
2022-05-03 11:32:31 +0100

    Initial Commit

---
+++ Recollect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recollect
+//
+//  Created by Sourabh Wasnik on 03/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
